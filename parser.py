971f015a622b048b489660dfb19ad23e33ea3bf1
Karabetskyi Heorhii <gkatabetskii@gmail.com>
2024-01-10 20:01:06 +0200

    The parser has been changed to asynchronous, the time has been accelerated by 7 times

--- parser.py
+++ parser.py
@@ -1,38 +1,42 @@
 """
-Module: realty_parser
+Module: parser
 
-This module defines a Parser class for extracting real estate data,
-from a list of links.
-The parsed data includes information such as address, region, description,
-pictures, price,number of bedrooms, and living area.
+This module defines an AsyncParser class for asynchronously extracting,
+real estate data,
+from a list of links. The parsed data includes information such as address,
+region, description, pictures, price, number of bedrooms, and living area.
 
 Classes:
-- Parser: A class for parsing real estate data from a list of links.
+- AsyncParser: An asynchronous class for parsing real estate data from,
+a list of links.
 
 Example Usage:
-    from realty_parser import Parser
+    from realty_parser import AsyncParser
     from data_saver import DataSaver
 
     # List of real estate links to parse
 
-    # Create an instance of the Parser class
-    parser = Parser()
+    # Create an instance of the AsyncParser class
+    async_parser = AsyncParser()
 
-    # Parse real estate data from the provided links
-    parser.parse_all_links(LIST_OF_ALL_LINKS)
-
-    # Create an instance of the DataSaver class with parsed data
-    data_saver = DataSaver(parser.data)
+    # Parse real estate data asynchronously from the provided links
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(
+        async_parser.parse_all_links_async(LIST_OF_ALL_LINKS)
+    )
+    # Create an instance of the DataSaver class with asynchronously parsed data
+    data_saver = DataSaver(async_parser.data)
 
     # Save parsed data to a JSON file
     data_saver.save_data_to_json()
 """
 
+import asyncio
 import logging
 from typing import List
 
+import aiohttp
 import bs4
-import requests
 
 from data_saver import DataSaver
 from get_all_links import LIST_OF_ALL_LINKS, HEADERS
@@ -43,18 +47,23 @@ from utils import (
     parse_living_area,
     parse_address,
     parse_description,
-    parse_page_title, initialize_soup,
+    parse_page_title,
+    initialize_soup,
 )
 
 
-class Parser:
+class AsyncParser:
 
     """
-    The Parser class is responsible for parsing real estate data,
-        from a list of links.
+    Asynchronous web scraper for real estate data.
+
+    This class is designed to asynchronously fetch and parse real estate data
+    from a list of links. It uses aiohttp for asynchronous HTTP requests and
+    BeautifulSoup for HTML parsing.
 
     Attributes:
-    - _base_headers (dict): Default headers for HTTP requests.
+    - log_file (str): The name of the log file for logging.
+    - log_level (int): The logging level. Defaults to logging.INFO.
     - data (list): List to store the parsed real estate data.
     """
 
@@ -62,27 +71,54 @@ class Parser:
         self, log_file: str = "parse_log.txt", log_level: int = logging.INFO
     ) -> None:
         """
-        Constructor method to initialize the Parser instance.
+        Initialize the AsyncParser instance.
 
         Parameters:
-        - log_file (str): The name of the log file. Defaults to 'parse_log.txt'.
+        - log_file (str): The name of the log file. Defaults,
+            to 'parse_log.txt'.
         - log_level (int): The logging level. Defaults to logging.INFO.
         """
         logging.basicConfig(filename=log_file, level=log_level)
         self.data = []
 
+    async def fetch_data(self, session, link):
+        """
+        Asynchronously fetch data from a given link using aiohttp session.
+
+        Parameters:
+        - session: aiohttp.ClientSession: The aiohttp session object.
+        - link (str): The link to fetch data from.
+        """
+        try:
+            async with session.get(link, headers=HEADERS) as response:
+                response.raise_for_status()
+                html = await response.text()
+                soup = initialize_soup(html)
+                self.parse_link(link, soup)
+        except aiohttp.ClientError as e:
+            logging.error(f"Failed to fetch data for link {link}. Error: {e}")
+
+    async def parse_all_links_async(self, links: List[str]):
+        """
+        Asynchronously parse real estate data from a list of links.
+
+        Parameters:
+        - links (List[str]): List of links to real estate properties.
+        """
+        async with aiohttp.ClientSession() as session:
+            tasks = [self.fetch_data(session, link) for link in links]
+            await asyncio.gather(*tasks)
+
     def parse_link(self, link: str, soup: bs4.BeautifulSoup) -> None:
         """
-        Parses real estate data from a given link and adds it to the data list.
+        Parse real estate data from a given link and add it to the data list.
 
         Parameters:
         - link (str): The link to the real estate property.
-        - soup (bs4.BeautifulSoup): BeautifulSoup object containing, real
-                estate information.
+        - soup (bs4.BeautifulSoup): BeautifulSoup object containing real,
+            estate information.
         """
         try:
-            response = requests.get(link, headers=HEADERS)
-            response.raise_for_status()
             count_bedrooms = parse_bedrooms(soup)
             living_area = parse_living_area(soup)
             photo_array = parse_photo_array(soup)
@@ -103,30 +139,24 @@ class Parser:
                     "Area": living_area,
                 }
             )
-        except requests.exceptions.RequestException as e:
-            logging.error(f"Failed to fetch data for link {link}. Error: {e}")
-
-    def parse_all_links(self, links: List[str]) -> None:
-        """
-        Parses real estate data from a list of links.
-
-        Parameters:
-        - links (list): List of links to real estate properties.
-        """
-        for link in links:
-            try:
-                response = requests.get(link, headers=HEADERS)
-                response.raise_for_status()
-                soup = initialize_soup(response.text)
-                self.parse_link(link, soup)
-            except requests.exceptions.RequestException as e:
-                logging.error(
-                    f"Failed to fetch data for link {link}. Error: {e}")
+        except Exception as e:
+            logging.error(f"Failed to parse data for link {link}. Error: {e}")
 
 
 if __name__ == "__main__":
-    parser = Parser()
-    parser.parse_all_links(LIST_OF_ALL_LINKS)
+    import time
 
-    data_saver = DataSaver(parser.data)
+    start_time = time.time()
+
+    async_parser = AsyncParser()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(
+        async_parser.parse_all_links_async(LIST_OF_ALL_LINKS)
+    )
+
+    data_saver = DataSaver(async_parser.data)
     data_saver.save_data_to_json()
+
+    end_time = time.time()
+    elapsed_time = end_time - start_time
+    print(f"Execution time: {elapsed_time} seconds")
